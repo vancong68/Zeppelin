147b459b3f620e8ee7cb54c451f6b4b1555d5060
vancong68 <vancong6868@gmail.com>
2026-04-12 23:15:40 +0700

    Handle member mute status without mute role
    Add check for communicationDisabledUntil when mute_role is not set.

--- backend/src/plugins/Mutes/functions/memberHasMutedRole.ts
+++ backend/src/plugins/Mutes/functions/memberHasMutedRole.ts
@@ -4,5 +4,10 @@ import { MutesPluginType } from "../types";
 
 export function memberHasMutedRole(pluginData: GuildPluginData<MutesPluginType>, member: GuildMember): boolean {
   const muteRole = pluginData.config.get().mute_role;
+  if (!muteRole) {
+    if (!member.communicationDisabledUntil) return false;
+    const diff = new Date(member.communicationDisabledUntil).getTime() - Date.now();
+    return diff > 10 * 1000;
+  }
   return muteRole ? member.roles.cache.has(muteRole as Snowflake) : false;
 }
